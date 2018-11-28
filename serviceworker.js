46b84de6f4ec6c8f763f8dccd3a6dbdef18bc7f2
skruschwitz <skruschwitz@adn.com>
2018-11-27 19:03:19 -0900

    added restaraunts explicitly

--- serviceworker.js
+++ serviceworker.js
@@ -1,12 +1,24 @@
 let theCache = 'reviews';
 let urlCacheList =
 ['/',
+'img/',
 'css/styles.css',
+'data/restaurants.json',
 'js/main.js',
 'js/dbhelper.js',
 'js/restaurant_info.js',
 'index.html',
-'restaurant.html'];
+'restaurant.html?id=1',
+'restaurant.html?id=2',
+'restaurant.html?id=3',
+'restaurant.html?id=4',
+'restaurant.html?id=5',
+'restaurant.html?id=6',
+'restaurant.html?id=7',
+'restaurant.html?id=8',
+'restaurant.html?id=9',
+'restaurant.html?id=10'
+]
 
 self.addEventListener('install', function(event) {
   // Perform install steps
@@ -33,6 +45,8 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+
 
 
   // event.respondWith(
